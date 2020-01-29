93d22ef7b8ed47b4cb14a4af7c2847973291fceb
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-01-29 15:30:01 -0300

    Reta final do curso

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
     phraseSizeUpdate()
     counterInit()
     chronoInit()
+    initMarks()
     $('#restart').click(restartGame)
 })
 
@@ -39,16 +40,38 @@ function counterInit() {
 function chronoInit() {
     var timeLeft = $('#time-digi').text()
     camp.one('focus', function() {
+        $("#botao-reiniciar").attr("disabled",true);
         var chronoID = setInterval(() => {
             timeLeft--
             $('#time-digi').text(timeLeft)
             if (timeLeft < 1) {
-                camp.attr('disabled',true)
-                // attr pega ou muda o atributo
                 clearInterval(chronoID)
-                // limpa o set interval
+                finishGame()
             }
         }, 1000);
+    })
+}
+
+function finishGame() {
+    camp.attr('disabled',true)
+    // attr pega ou muda o atributo
+    $("#botao-reiniciar").attr("disabled", false);
+    camp.addClass('disable-digi')
+    insertScore()
+}
+function initMarks() {
+    var phrase = $('.phrase').text()
+    camp.on('input',function() {
+    var typed = camp.val()
+    var comparable = phrase.substr(0,typed.length)
+    // substr é uma substring que pega parte da string do ponto inicial (primeiro parametro) até onde for determinado pelo segundo parâmetro
+    if (typed == comparable) {
+        camp.addClass('green-border')
+        camp.removeClass('red-border')
+    } else {
+        camp.addClass('red-border')
+        camp.removeClass('green-border')
+    }
     })
 }
 
@@ -60,5 +83,58 @@ function restartGame() {
     $('#counter-w').text('0')
     $('#counter-c').text('0')
     $('#time-digi').text(initialTime)
-    chronoInit()  
+    chronoInit()
+    camp.removeClass('disable-digi')
+    camp.removeClass('red-border')
+    camp.removeClass('green-border')
 }
+
+function insertScore() {
+    var tbody = $('.score').find('tbody')
+    // .find() procura o que for passado como parametro dentro da arvore
+    var user = 'Tina'
+    var numWords = $('#counter-w').text()
+
+    var line = newTr(user, numWords)
+    line.find('.remove').click(rmLine)
+    tbody.prepend(line)
+
+}
+
+function newTr(user, words) {
+    var line = $('<tr>')
+    var colUser = $('<td>').text(user)
+    var colWords = $('<td>').text(words)
+    var colRemove = $('<td>')
+
+    var link = $('<a>').addClass('remove').attr('href', '#')
+    var icon = $('<i>').addClass('small').addClass('material-icons').text('delete-forever')
+
+    link.append(icon)
+    colRemove.append(link)
+
+    line.append(colUser)
+    line.append(colWords)
+    line.append(colRemove)
+}
+
+function rmLine(event) {
+    event.preventDefault()
+    $(this).parent().parent().parent().remove()
+}
+
+// Versão enxuta da initMarks
+
+// function initMarks() {
+//     var phrase = $('.phrase').text()
+//     var typed = camp.val()
+//     camp.on('input',function() {
+//         if (phrase.startsWith(typed)) {
+//             camp.addClass('green-border')
+//             camp.removeClass('red-border')
+//         } else {
+//             camp.addClass('red-border')
+//             camp.removeClass('green-border')
+//         }
+//     })
+// }
