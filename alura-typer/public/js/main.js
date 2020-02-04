f4edd902163cfe0ca617f240ad2fcb3e8823a2c5
Nhemetz <marcelle.nhemetz@gmail.com>
2020-02-04 16:54:23 -0300

    Separando código

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -89,44 +89,6 @@ function restartGame() {
     camp.removeClass('green-border')
 }
 
-function insertScore() {
-    var tbody = $('.score').find('tbody')
-    // .find() procura o que for passado como parametro dentro da arvore
-    var user = 'Tina'
-    var numWords = $('#counter-w').text()
-
-    var line = newTr(user, numWords)
-    line.find('.remove-button').click(removeLine)
-    tbody.prepend(line)
-
-}
-
-function newTr(user, words) {
-    var line = $('<tr>')
-    var colUser = $('<td>').text(user)
-    var colWords = $('<td>').text(words)
-    var colRemove = $('<td>')
-
-    var link = $('<a>').addClass('remove-button').attr('href', '#')
-    var icon = $('<i>').addClass('small').addClass('material-icons').text('delete_forever')
-
-    link.append(icon)
-    colRemove.append(link)
-
-    line.append(colUser)
-    line.append(colWords)
-    line.append(colRemove)
-    
-    return line
-}
-
-$(".remove-button").click(removeLine)
-
-function removeLine(event) {
-    event.preventDefault()
-    $(this).parent().parent().remove()
-}
-
 // Versão enxuta da initMarks
 
 // function initMarks() {
