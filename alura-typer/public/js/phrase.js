f334ebd8d638032f71cb2a6b34426ecae01fbac8
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-02-12 19:27:25 -0300

    tentando arrumar arquivos

--- alura-typer/public/js/phrase.js
+++ alura-typer/public/js/phrase.js
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
 $('#button-phrase').click(randomPhrase)
 $('#button-search').click(searchPhrase)
-=======
-$('#phrase').click(randomPhrase)
-$('#search').click(searchPhrase)
->>>>>>> Stashed changes
 
 /* ~ Função do botão Phrase para trazer uma frase aleatória ao jogo! ~ */
 function randomPhrase() {
@@ -29,7 +24,6 @@ function shuffleRandomPhrase(data) {
     let randomNum = Math.floor(Math.random() * data.length)
     phrase.text(data[randomNum].texto)
     phraseSizeUpdate()
-<<<<<<< Updated upstream
     initTimeUpdate(data[randomNum].tempo)
 }
 
@@ -38,15 +32,6 @@ function searchPhrase() {
     $('#spinner').toggle()
     let idPhrase = $('#phrase-id').val()
     let data = { id: idPhrase }
-=======
-    initTimeUpdate(data[randomNum].tempo);
-}
-
-function searchPhrase() {
-    $('#spinner').toggle()
-    var idPhrase = $('#id-phrase').val()
-    var data = {id: idPhrase}
->>>>>>> Stashed changes
     $.get('http://localhost:3000/frases', data, shufflePhrase)
     .fail(() => {
         $('#error').toggle()
@@ -58,16 +43,10 @@ function searchPhrase() {
         $('#spinner').toggle()
     })
 }
-<<<<<<< Updated upstream
 /* ~ Função que mostra qual frase a função searchPhrase buscou no servidor! ~ */
 function shufflePhrase(data) {
     let phrase = $('.phrase')
     phrase.text(data.texto)
     phraseSizeUpdate()
     initTimeUpdate(data.tempo)
-=======
-
-function shufflePhrase(data) {
-    console.log(data)
->>>>>>> Stashed changes
 }
